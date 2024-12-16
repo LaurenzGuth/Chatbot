d02dc4801c7305e6f8eb2c37266cccba0aa15045
laurenz <laurenz.g@gmx.de>
2024-12-16 17:07:18 +0100

    Weitere Übung funktinoiert fast. die erste Übung kann doppelt gezeigt werden.

--- actions/actions.py
+++ actions/actions.py
@@ -1,14 +1,4 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa-pro/concepts/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
 from pymongo import MongoClient
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -22,25 +12,29 @@ class ActionSuggestExercise(Action):
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client["exercise_database"]
         self.exercises_collection = self.db["exercises"]
+        self.last_exercises = []
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         weakness = tracker.get_slot("weakness")
-
         user_level = ''.join(filter(str.isdigit, tracker.get_slot("level")))[:1]
-
         level_range = [int(user_level) - 1, int(user_level), int(user_level) + 1]
 
         exercise = self.exercises_collection.find_one({
             "level": {"$in": level_range},
-            "tags": weakness
+            "tags": weakness,
+            "_id": {"$nin": self.last_exercises}
         })
+
         if exercise:
+            self.last_exercises.append(exercise["_id"])
+            if len(self.last_exercises) > 5:
+                self.last_exercises.pop(0)
             message = f"Hier ist eine Übung für dich: {exercise['name']} - {exercise['description']}"
         else:
-            message = "Leider habe ich keine Übung für dein Level gefunden." + user_level + weakness
+            message = "Leider habe ich keine Übung für dein Level gefunden."
 
         dispatcher.utter_message(text=message)
         return []
@@ -54,19 +48,59 @@ class ActionSuggestExerciseByType(Action):
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client["exercise_database"]
         self.exercises_collection = self.db["exercises"]
+        self.last_exercises = []
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        askexercise = tracker.get_slot("askExercise")
+        weakness = tracker.get_slot("weakness")
 
-        exercise = self.exercises_collection.find_one({"tags": askexercise})
+        exercise = self.exercises_collection.find_one({
+            "tags": weakness,
+            "_id": {"$nin": self.last_exercises}
+        })
 
         if exercise:
-            message = f"Hier ist eine Übung für {askexercise}: {exercise['name']} - {exercise['description']}"
+            self.last_exercises.append(exercise["_id"])
+            if len(self.last_exercises) > 5:
+                self.last_exercises.pop(0)
+            message = f"Hier ist eine Übung für {weakness}: {exercise['name']} - {exercise['description']}"
         else:
-            message = f"Leider habe ich keine Übung für {askexercise} gefunden."
+            message = f"Leider habe ich keine Übung für {weakness} gefunden."
+
+        dispatcher.utter_message(text=message)
+        return []
+
+
+class ActionSuggestAnotherExercise(Action):
+    def name(self) -> Text:
+        return "action_suggest_another_exercise"
+
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["exercise_database"]
+        self.exercises_collection = self.db["exercises"]
+        self.last_exercises = []
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        weakness = tracker.get_slot("weakness")
+
+        exercise = self.exercises_collection.find_one({
+            "tags": weakness,
+            "_id": {"$nin": self.last_exercises}
+        })
+
+        if exercise:
+            self.last_exercises.append(exercise["_id"])
+            if len(self.last_exercises) > 5:
+                self.last_exercises.pop(0)
+            message = f"Hier ist eine weitere Übung für {weakness}: {exercise['name']} - {exercise['description']}"
+        else:
+            message = f"Leider habe ich keine weitere Übung für {weakness} gefunden."
 
         dispatcher.utter_message(text=message)
         return []
